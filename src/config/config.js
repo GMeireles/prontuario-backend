bf19bc833146b98197f16db2ace69a75d79d6f46
root <root@srv881417.hstgr.cloud>
2025-09-12 23:10:48 +0000

    Por algum motivo, sem o || na config, mysql da erro de native_password

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,9 @@
 export default {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    username: process.env.DB_USER || "guilherme",
+    password: process.env.DB_PASS || "Loveforever@2003",
+    database: process.env.DB_NAME || "prontuario_development",
+    host: process.env.DB_HOST || "127.0.0.1",
     dialect: "mysql"
   },
   production: {
