5f0eed83cd0755d15bcb174a54f6e8a5451298f3
root <root@srv881417.hstgr.cloud>
2025-09-20 17:11:43 +0000

    Alteração no export para pegar config de banco do .env

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,11 @@
+import 'dotenv/config';
+
 export default {
   development: {
-    username: process.env.DB_USER || "guilherme",
-    password: process.env.DB_PASS || "Loveforever@2003",
-    database: process.env.DB_NAME || "saas_prontuario_production",
-    host: process.env.DB_HOST || "127.0.0.1",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: "mysql"
   },
   production: {
